41800345a007c103b1e7a14985164a976afafe08
Ali-javed123 <muhammadaliadil999@gmail.com>
2026-04-10 06:20:47 +0500

    vbvn

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = ( err, req, res, next ) => {
     console.error( err.stack );
-    res.status( 500 ).json( { error: err.message || 'Internal Server Error' } );
+    res.status( 500 ).json( { error: err.message || 'Internal Server Errors' } );
 };
 
 export default errorHandler;
