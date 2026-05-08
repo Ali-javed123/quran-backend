c3e81e563c2466e4325a70df602289e476461850
ALi-javed123 <muhammadaliadil999@gmail.com>
2026-05-09 03:53:09 +0500

    SDFSDWE

--- server.js
+++ server.js
@@ -262,5 +262,5 @@ app.use(errorHandler);
 // ─────────────────────────────────────────
 await connectDB(); // ⛔ WAIT HERE (main fix)
     server.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Server running on port ${PORT}`);
+      console.log(`🚀 Server running on ports ${PORT}...`);
 });
